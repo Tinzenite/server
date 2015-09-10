175a709f861af90fdc32016ad7814f075eef0f29
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-09-10 17:53:08 +0200

    small start

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+)
 
 func main() {
-	log.Println("Hello world!")
+	log.Println("Starting server.")
+	// define required flags
+	var path string
+	flag.StringVar(&path, "path", "temp", "File directory path in which to run the server.")
+	// parse flags
+	flag.Parse()
+	// TODO check & ask whether to create if none currently exists (also add flag for this?)
+	// TODO if path wasn't given, ask for it (see shared code from tinzenite/tin)
+	log.Println("Path:", path)
 }
