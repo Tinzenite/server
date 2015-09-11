e65e88c5704cdf0b7221325eb6d048e88f4b004d
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-09-11 17:08:45 +0200

    basic channel runs

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+
+	"github.com/tinzenite/encrypted"
+	"github.com/tinzenite/shared"
 )
 
 func main() {
@@ -15,4 +21,28 @@ func main() {
 	// TODO check & ask whether to create if none currently exists (also add flag for this?)
 	// TODO if path wasn't given, ask for it (see shared code from tinzenite/tin)
 	log.Println("Path:", path)
+
+	if exists, _ := shared.DirectoryExists(path); !exists {
+		shared.MakeDirectory(path)
+	}
+
+	enc, err := encrypted.Create(path, "d_server")
+	if err != nil {
+		log.Println("Server: failed to create:", err)
+		return
+	}
+	// print important info
+	address, _ := enc.Address()
+	fmt.Printf("Running peer <%s>.\nID: %s\n", enc.Name(), address)
+	// prepare quitting via ctrl-c
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	// loop until close
+	for {
+		select {
+		case <-c:
+			log.Println("Server: quitting.")
+			return
+		} // select
+	} // for
 }
